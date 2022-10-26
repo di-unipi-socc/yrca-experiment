897abfe7a19da4ef30afb2a9dc4a29469807d42f
Luca Roveroni <74104490+LucaRUnipi@users.noreply.github.com>
2022-10-26 10:48:28 +0200

    Delete scripts directory

--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-### Run the full Online Boutique Stack with Logstash
-docker stack deploy -c docker-compose.yml onlineBoutique
-
-printf "\nYou can access Online Boutique at http://131.114.50.191:8888\n"
